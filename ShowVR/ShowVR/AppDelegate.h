dcd3f129bcf9de24b1663193f9d022f911acc415
shaoxu <892915662@qq.com>
2016-11-04 14:48:22 +0800

    vr

---
+++ ShowVR/ShowVR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowVR
+//
+//  Created by shaoxu on 2016/11/4.
+//  Copyright © 2016年 sx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
